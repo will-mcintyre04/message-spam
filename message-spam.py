53ed1366c967db08b3f07a83d79eb4a2dbcb3f1b
will-mcintyre04 <william.d.j.mcintyre@gmail.com>
2021-02-08 19:13:38 -0500

    Create message-spam.py

---
+++ message-spam.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 18:06:24 2021
+
+@author: williammcintyre
+"""
+
+# Spam Messages Code
+# Will McIntyre
+# February 8th, 2021
+
